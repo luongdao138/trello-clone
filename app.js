80a6fc4bb72f84b3132b2ee05206b185792e729f
Dao Luong <daovanluongpdl@gmail.com>
2021-10-06 07:58:55 +0700

    finish

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'https://wizardly-nightingale-2253f3.netlify.app/',
+      'https://wizardly-nightingale-2253f3.netlify.app',
     ],
   })
 );
